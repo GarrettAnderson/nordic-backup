3b175bcb4c205cb2e694fb1b4db501bd9a98255e
Garrett Anderson <garrettleegrahamanderson@gmail.com>
2022-08-25 19:47:34 -0400

    got the pagination for prev page and next page

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,37 @@ export class App extends Component {
     offset: 0,
   }
 
+  fetchPrevPage() {
+    const currentOffset = this.state.offset - 50
+    fetch(
+      `http://localhost:8080/data/index.php?offset=${encodeURIComponent(
+        currentOffset
+      )}`,
+      {
+        // mode: 'no-cors',
+        // method: 'GET',
+        // headers: { 'Content-Type': 'application/json' },
+      }
+    )
+      .then(resp => {
+        console.log(resp)
+        return resp.json()
+      })
+      .then(json => {
+        // const data = JSON.parse(text);
+        console.log(json)
+        console.log(json.length + currentOffset)
+        this.setState({
+          data: json,
+          offset: currentOffset,
+        })
+        console.log(this.state)
+      })
+      .catch(e => {
+        console.error(e)
+      })
+  }
+
   fetchNextPage() {
     const currentOffset = this.state.offset
     fetch(
@@ -28,7 +59,7 @@ export class App extends Component {
         console.log(json)
         this.setState({
           data: json,
-          offset: json.size + currentOffset,
+          offset: json.length + currentOffset,
         })
         console.log(this.state)
       })
@@ -57,7 +88,7 @@ export class App extends Component {
         console.log(json)
         this.setState({
           data: json,
-          offset: json.size,
+          offset: json.length,
         })
       })
       .catch(e => {
@@ -69,24 +100,37 @@ export class App extends Component {
     return (
       <>
         <table>
-          <tr>
-            <th>Customer Id</th>
-            <th>Client Id</th>
-            <th>Job Id</th>
-            <th>Backupset</th>
-            <th>Day and Time</th>
-          </tr>
-          {this.state.data.map((datum, index) => {
-            return <TableRow data={datum} index={index} />
-          })}
-          <tr>
+          <tbody>
+            <tr>
+              <th>Customer Id</th>
+              <th>Client Id</th>
+              <th>Job Id</th>
+              <th>Backupset</th>
+              <th>Day and Time</th>
+            </tr>
+            {this.state.data.map((datum, index) => {
+              return <TableRow data={datum} key={index} />
+            })}
+          </tbody>
+        </table>
+        <div>
+          {this.state.offset > 50 ? (
             <button
               onClick={() => {
-                this.fetchNextPage()
+                this.fetchPrevPage()
               }}
-            >Next page</button>
-          </tr>
-        </table>
+            >
+              Prev page
+            </button>
+          ) : null}
+          <button
+            onClick={() => {
+              this.fetchNextPage()
+            }}
+          >
+            Next page
+          </button>
+        </div>
       </>
     )
   }
